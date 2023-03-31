1c83a5fb5f178a3414724468aa50280ae3d81318
Divyansh-buddy <121878051+Divyansh-buddy@users.noreply.github.com>
2023-03-31 13:40:10 +0000

    Update 1.1.1

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+var menu = document.getElementById('menu');
+var navbar = document.querySelector('.navbar');
+var timeoutId;
 
+navbar.style.display = "none";
+
+menu.addEventListener('click', function() {
+  if (navbar.style.display === "none") {
+    navbar.style.display = "flex";
+  } else {
+    navbar.style.display = "none";
+    clearTimeout(timeoutId);
+  }
+
+  if (navbar.style.display === "flex") {
+    timeoutId = setTimeout(function() {
+      navbar.style.display = "none";
+    }, 8500);
+  }
+});
